4a03da2a9ae189a926b6d7c01b3760e4e8f53ab2
Davut Temel <developerpc@95.1.100.164.dynamic.ttnet.com.tr>
2018-12-10 15:18:09 +0300

    initial commit

---
+++ OyuncuSepeti.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using OyuncuSepeti.API.Models;
+
+namespace OyuncuSepeti.API.Data
+{
+    public class DataContext : DbContext
+    { public DataContext(DbContextOptions<DataContext> options): base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
