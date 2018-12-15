9c1001762863fa86fcdcb08af31886b6ed40290f
Davut Temel <developerpc@Davut-MacBook-Pro.local>
2018-12-15 20:56:27 +0300

    loginveregister yapıldı

--- OyuncuSepeti.API/Controllers/ValuesController.cs
+++ OyuncuSepeti.API/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace OyuncuSepeti.API.Controllers
             _context = context;
 
         }
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
